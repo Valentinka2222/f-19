145c81c8c771e37ebaffa8ddd21de46d8f70faf7
Valentinka2222 <xamalyan89@gmail.com>
2022-06-08 13:50:34 +0300

    3

--- react/task2/src/Pagination.jsx
+++ react/task2/src/Pagination.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
-const Pagination = ({ currentPage, goPrev, goNext, totalItems }) => {
+const Pagination = ({ currentPage, goPrev, goNext, totalItems, itemsPerPage }) => {
   const isPrevPageAvailable = '←';
-  const isNextPageAvailable = '→';
+  let isNextPageAvailable = '→';
+  let disabled;
+  if (totalItems % 3) {
+    disabled = true;
+    isNextPageAvailable = null;
+  } else {
+    disabled = false;
+  }
   return (
     <div className="pagination">
       <button className="btn" onClick={goPrev}>
         {currentPage <= 1 ? null : isPrevPageAvailable}
       </button>
       <span className="pagination__page">{currentPage}</span>
-      <button className="btn" onClick={goNext} disabled={totalItems % 3 ? true : false}>
+      <button className="btn" onClick={goNext} disabled={disabled}>
         {isNextPageAvailable}
       </button>
     </div>
